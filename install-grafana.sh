0da55acd32acdf5eacc46e4b3d0f64b2d79a4ccd
itsmeshabeeb <mshabeeb4@gmail.com>
2022-11-21 12:22:26 +0530

    fixed

--- install-grafana.sh
+++ install-grafana.sh
@@ -3,5 +3,5 @@ sudo cp grafana.repo /etc/yum.repos.d/
 
 sudo yum install grafana -y
 sudo systemctl start grafana-server
-sudo systemctl status grafana-server
 sudo systemctl enable grafana-server.service
+sudo systemctl status grafana-server
